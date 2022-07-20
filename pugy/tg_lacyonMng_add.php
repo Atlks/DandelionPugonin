a08240742b40aeeb6a4fea5239da738c02e5c8f3
domas <your_email@example.com>
2022-07-20 17:21:30 +0800

    ...

---
+++ pugy/tg_lacyonMng_add.php
@@ -0,0 +1,27 @@
+<?php
+   echo  __DIR__.'/conn_db.php';
+include './conn_db.php';
+$dtm=date("Y-m-d H:i:s");
+//https://t.me/qingliao
+$grp=$_GET['grp'];$freq=$_GET['freq'];$imp=$_GET['imp'];
+if(!$_GET['freq'])
+$freq=0;
+$botnum=(int)$_GET['botnum'];
+//"imp":$imp,
+
+$values = array( '$grp' => $grp, '$freq' => $freq,'$imp' => $imp);
+$fld='{"id":1,  "grp":"$grp","id":1,"yicaiji":10,"freq":$freq,"cloudsvr":"1号服务器","stat":"进行中","updt":"'.$dtm.'","imp":10,"av":9}';
+ $fld=strtr($fld, $values);
+echo $fld;
+ //die();
+$sql="insert tbx set flds='$fld',typex='lacyonMng'";
+echo $sql;
+$r=pdo_exec($sql);
+echo $r;
+
+
+?>
+<script>
+window.location='tg_lacyonMng.htm'
+
+</script>
